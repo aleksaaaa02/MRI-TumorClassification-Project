ed183b65d5670eaff02b7d24d363e484d4365179
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2024-06-27 18:52:56 +0200

    version 2

--- main.py
+++ main.py
@@ -6,26 +6,33 @@ import matplotlib.pyplot as plt
 from tensorflow import data as tf_data
 from keras.src.utils import load_img, img_to_array
 from keras.src.optimizers.adam import Adam
+from keras.src.optimizers.schedules import ExponentialDecay
 from keras.src.losses.losses import CategoricalCrossentropy, SparseCategoricalCrossentropy
 from keras.src.models import Sequential
-from keras.src.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from keras_cv.src.layers import RandomFlip, RandomRotation, Rescaling, Equalization, Posterization
+from keras.src.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, GlobalAveragePooling2D
+from keras_cv.src.layers import Equalization
+from keras.src.models import Model
+from keras.src.layers import BatchNormalization, RandomZoom, RandomTranslation, RandomBrightness, RandomFlip, RandomRotation, Rescaling
 from keras.src.utils.image_dataset_utils import image_dataset_from_directory
 from keras.src.callbacks import ModelCheckpoint, EarlyStopping
-
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report
+from keras.src.applications.efficientnet import EfficientNetB0
 
 BATCH_SIZE = 32
-IMG_HEIGHT = 180
-IMG_WIDTH = 180
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
 AUTOTUNE = tf.data.AUTOTUNE
-EPOCHS = 25
+EPOCHS = 15
 
 SAVE_PROGRESS = True
 
-training_dataset_url = "C:\\Fakultet\\Semestar 6\\Racunarska Inteligencija\\Projekat\\Data\\Training"
-testing_dataset_url = "C:\\Fakultet\\Semestar 6\\Racunarska Inteligencija\\Projekat\\Data\\Testing"
+training_dataset_url = "C:/Fakultet/Semestar 6/Racunarska Inteligencija/Projekat/Data/Training"
+testing_dataset_url = "C:/Fakultet/Semestar 6/Racunarska Inteligencija/Projekat/Data/Testing"
 
-prediction_image_url = "C:\\Fakultet\\Semestar 6\\Racunarska Inteligencija\\Projekat\\glioma_test.jpeg"
+prediction_image_url = "C:/Fakultet/Semestar 6/Racunarska Inteligencija/Projekat/meningioma_test.jpg"
+
+labels = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
 
 class_names = {
     0: 'glioma_tumor',
@@ -39,9 +46,8 @@ NUM_CLASSES = 4
 data_augmentation_layers = [
     RandomFlip("horizontal"),
     RandomRotation(0.1),
-    Equalization([0, 255], 256),
-    # Posterization(value_range=[0, 160], bits=7),
-    Rescaling(scale=1./255)
+    RandomZoom(0.2),
+    RandomBrightness(factor=0.2)
 ]
 
 
@@ -49,12 +55,16 @@ def build_model():
     model = Sequential([
         keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 1)),
         Conv2D(16, 3, padding='same', activation='relu'),
+        BatchNormalization(),
         MaxPooling2D(),
         Conv2D(32, 3, padding='same', activation='relu'),
+        BatchNormalization(),
         MaxPooling2D(),
         Conv2D(64, 3, padding='same', activation='relu'),
+        BatchNormalization(),
         MaxPooling2D(),
         Conv2D(128, 3, padding='same', activation='relu'),
+        BatchNormalization(),
         MaxPooling2D(),
         Flatten(),
         Dense(128, activation='relu'),
@@ -62,22 +72,25 @@ def build_model():
         Dense(NUM_CLASSES, activation='softmax', name='outputs')
     ])
     return model
+def use_pretrained_model():
+    # Load EfficientNet-B0 pre-trained on ImageNet without top layers
+    base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3))
 
+    # Add custom classification head
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(128, activation='relu')(x)
+    predictions = Dense(NUM_CLASSES, activation='softmax')(x)
+
+    # Combine base model and custom head into a single model
+    model = Model(inputs=base_model.input, outputs=predictions)
+    return model
 
 def train_and_evaluate():
-
-    train_dataset = image_dataset_from_directory(
+    train_dataset, val_dataset = image_dataset_from_directory(
         training_dataset_url,
-        label_mode='categorical',
-        color_mode='grayscale',
-        shuffle=True,
-        seed=1337,
-        image_size=(IMG_HEIGHT, IMG_WIDTH),
-        batch_size=BATCH_SIZE
-    )
-
-    val_dataset = image_dataset_from_directory(
-        testing_dataset_url,
+        validation_split=0.2,
+        subset="both",
         label_mode='categorical',
         color_mode='grayscale',
         shuffle=True,
@@ -95,31 +108,32 @@ def train_and_evaluate():
     val_dataset = val_dataset.prefetch(tf_data.AUTOTUNE)
 
     # Creating a model
-    generated_model = build_model()
+    generated_model = use_pretrained_model()
 
-    generated_model.compile(optimizer=Adam(learning_rate=0.0001),
+    lr_schedule = ExponentialDecay(
+        initial_learning_rate=0.001,
+        decay_steps=10000,
+        decay_rate=0.9
+    )
+
+    generated_model.compile(optimizer=Adam(lr_schedule),
                             loss=CategoricalCrossentropy(from_logits=False),
                             metrics=['accuracy'])
 
-    # early_stopping_callback = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-    # callbacks = [early_stopping_callback]
-    # if SAVE_PROGRESS:
-    #     checkpoint_callback = ModelCheckpoint(filepath='best_model.keras', save_best_only=True, monitor='val_loss', mode='min')
-    #     callbacks.append(checkpoint_callback)
+    early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+    callbacks = [early_stopping_callback]
+    if SAVE_PROGRESS:
+        checkpoint_callback = ModelCheckpoint(filepath='best_model.keras', save_best_only=True, monitor='val_loss', mode='min')
+        callbacks.append(checkpoint_callback)
 
     generated_model.summary()
-    callbacks = []
     history = generated_model.fit(
             train_dataset,
             validation_data=val_dataset,
             epochs=EPOCHS,
             callbacks=callbacks)
 
-    if SAVE_PROGRESS:
-        generated_model.save('my_model.keras')
-
-    # plot_training_history(history)
-
+    plot_training_history(history)
     return generated_model
 
 
@@ -130,7 +144,7 @@ def plot_training_history(history):
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
-    epochs_range = range(EPOCHS)
+    epochs_range = range(len(acc))
 
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
@@ -158,21 +172,13 @@ def evaluate_test_dataset(trained_model=None):
 
     if trained_model is None:
         trained_model = keras.src.models.model.saving_api.load_model('my_model.keras')
-    # testing_dataset = testing_dataset.prefetch(buffer_size=AUTOTUNE)
 
-    # Need changing, should choose a picture than do a prediction on it
     predictions = trained_model.predict(input_arr)
 
     score = tf.nn.softmax(predictions[0])
     print(
         f"This image most likely belongs to {class_names[np.argmax(score)]} with a {100 * np.max(score):.2f} percent confidence.")
     print(predictions)
-
-
-def filter_corrupted_images():
-    num_skipped = 0
-    for folder_name in (""):
-        pass
 
 
 def data_augmentation(images):
@@ -187,7 +193,7 @@ def visualize_augmentation(dataset):
         for i in range(9):
             augmented_images = data_augmentation(images)
             ax = plt.subplot(3, 3, i + 1)
-            plt.imshow(np.array(augmented_images[i]).astype("uint8"))
+            plt.imshow(np.array(augmented_images[i]).astype("uint8"), "gray")
             plt.axis("off")
     plt.show()
 
@@ -197,16 +203,67 @@ def visualize_data(dataset):
     for images, labels in dataset.take(1):
         for i in range(9):
             ax = plt.subplot(3, 3, i + 1)
-            plt.imshow(np.array(images[i]).astype("uint8"))
+            plt.imshow(np.array(images[i]).astype("uint8"), "gray")
             plt.title(class_names[np.argmax(labels[i])])
             plt.axis("off")
     plt.show()
 
 
+def evaluate_model():
+    testing_dataset = image_dataset_from_directory(
+        testing_dataset_url,
+        label_mode='categorical',
+        color_mode='grayscale',
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+    )
+    trained_model = keras.src.models.model.saving_api.load_model('my_model.keras')
+    accuracy = trained_model.evaluate(testing_dataset)
+    print(accuracy)
+
+def plot_confusion_matrix(cm, class_names):
+    plt.figure(figsize=(8, 8))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+def evaluate_and_print_statistics(model=None):
+    # Get the true labels and predictions
+    if model is None:
+        model = keras.src.models.model.saving_api.load_model('best_model.keras')
+    test_dataset = image_dataset_from_directory(
+        testing_dataset_url,
+        shuffle=True,
+        label_mode='categorical',
+        color_mode='grayscale',
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCH_SIZE
+    )
+
+    true_labels = []
+    predictions = []
+
+    for images, labels in test_dataset:
+        print(images, labels)
+        preds = model.predict(images)
+        true_labels.extend(np.argmax(labels.numpy(), axis=1))
+        predictions.extend(np.argmax(preds, axis=1))
+
+    true_labels = np.array(true_labels)
+    predictions = np.array(predictions)
+
+    # Compute confusion matrix
+    cm = confusion_matrix(true_labels, predictions)
+    plot_confusion_matrix(cm, list(class_names.values()))
+
+    # Compute additional metrics
+    print("\nClassification Report:\n")
+    print(classification_report(true_labels, predictions, target_names=list(class_names.values())))
+
 if __name__ == "__main__":
     print("Hello World from my project!")
     print(tf.keras.__version__)
 
-    # model = train_and_evaluate()
-
-    evaluate_test_dataset()
+    model = train_and_evaluate()
+    evaluate_and_print_statistics(model)
